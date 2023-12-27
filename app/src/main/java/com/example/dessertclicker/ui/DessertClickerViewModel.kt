c163771c9f23372522a70b90104f93ddd4accd11
Courtlyn Boykin <courtlynb@google.com>
2023-12-27 13:26:01 -0600

    attempting to Relocate the app logic and data to the ViewModel

--- app/src/main/java/com/example/dessertclicker/ui/DessertClickerViewModel.kt
+++ app/src/main/java/com/example/dessertclicker/ui/DessertClickerViewModel.kt
@@ -12,5 +12,45 @@ class DessertClickerViewModel : ViewModel() {
     private val _uiState = MutableStateFlow(DessertUiState())
     val uiState: StateFlow<DessertUiState> = _uiState.asStateFlow()
 
+//    how would i know what goes into the view model?
+
+//In the context of your Dessert Clicker app, you can determine what goes into the ViewModel by identifying the data and logic that is responsible for managing the UI state and business logic. In the provided code, it seems like the DessertUiState class is a good candidate for the ViewModel's state, and the functions related to determining the dessert to show and sharing dessert information can be part of the ViewModel's logic.
+
+//Here's a suggested approach to organize your ViewModel:
+
+
+//class DessertClickerViewModel : ViewModel() {
+//    // Define a MutableStateFlow to hold the UI state
+//    private val _uiState = MutableStateFlow(DessertUiState())
+//    val uiState = _uiState.asStateFlow()
+//
+//    // Function to handle dessert click logic
+//    fun onDessertClicked(desserts: List<Dessert>) {
+//        // Update the revenue and dessertsSold
+//        _uiState.value = _uiState.value.copy(
+//            revenue = _uiState.value.revenue + _uiState.value.currentDessertPrice,
+//            dessertsSold = _uiState.value.dessertsSold + 1
+//        )
+//
+//        // Determine the next dessert to show
+//        viewModelScope.launch {
+//            val dessertToShow = determineDessertToShow(desserts, _uiState.value.dessertsSold)
+//            _uiState.value = _uiState.value.copy(
+//                currentDessertPrice = dessertToShow.price,
+//                currentDessertImageId = dessertToShow.imageId
+//            )
+//        }
+//    }
+//
+//    // Function to handle sharing dessert information
+//    fun onShareButtonClicked(context: Context) {
+//        shareSoldDessertsInformation(
+//            intentContext = context,
+//            dessertsSold = _uiState.value.dessertsSold,
+//            revenue = _uiState.value.revenue
+//        )
+//    }
+//}
+
 
 }
