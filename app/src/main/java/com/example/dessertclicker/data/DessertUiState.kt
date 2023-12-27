fb21eb50649b5c6a288769e753fdffa92d22f9a9
Courtlyn Boykin <courtlynb@google.com>
2023-12-27 16:40:38 -0600

    added logic and data to viewmodel

--- app/src/main/java/com/example/dessertclicker/data/DessertUiState.kt
+++ app/src/main/java/com/example/dessertclicker/data/DessertUiState.kt
@@ -7,7 +7,7 @@ data class DessertUiState (
     val currentDessertIndex: Int = 0,
     var revenue: Int = 0,
     var dessertsSold: Int = 0,
-    val currentDessertPrice: Int = dessertList[currentDessertIndex].price,
+    var currentDessertPrice: Int = dessertList[currentDessertIndex].price,
     @DrawableRes
-    val currentDessertImageId: Int = dessertList[currentDessertIndex].imageId
+    var currentDessertImageId: Int = dessertList[currentDessertIndex].imageId
 )
--- app/src/main/java/com/example/dessertclicker/ui/DessertClickerViewModel.kt
+++ app/src/main/java/com/example/dessertclicker/ui/DessertClickerViewModel.kt
@@ -1,16 +1,55 @@
 package com.example.dessertclicker.ui
 
 import androidx.lifecycle.ViewModel
+import com.example.dessertclicker.data.Datasource
 import com.example.dessertclicker.data.DessertUiState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import com.example.dessertclicker.model.Dessert
 
 class DessertClickerViewModel : ViewModel() {
 
     //Dessert UI State
     private val _uiState = MutableStateFlow(DessertUiState())
     val uiState: StateFlow<DessertUiState> = _uiState.asStateFlow()
+
+
+
+    /**
+     * Determine which dessert to show.
+     */
+    fun determineDessertToShow(
+        desserts: List<Dessert>,
+        dessertsSold: Int
+    ): Dessert {
+        var dessertToShow = desserts.first()
+        for (dessert in desserts) {
+            if (dessertsSold >= dessert.startProductionAmount) {
+                dessertToShow = dessert
+            } else {
+                // The list of desserts is sorted by startProductionAmount. As you sell more desserts,
+                // you'll start producing more expensive desserts as determined by startProductionAmount
+                // We know to break as soon as we see a dessert who's "startProductionAmount" is greater
+                // than the amount sold.
+                break
+            }
+        }
+
+        return dessertToShow
+    }
+
+    fun updateAppState() {
+        val desserts = Datasource.dessertList
+        val dessertToShow = determineDessertToShow(desserts, uiState.value.dessertsSold)
+        
+        _uiState.value = uiState.value.copy(
+                                     revenue = uiState.value.revenue + dessertToShow.price,
+                                     dessertsSold = uiState.value.dessertsSold++,
+                                     currentDessertPrice = dessertToShow.price,
+                                     currentDessertImageId = dessertToShow.imageId
+                                 )
+    }
 
 //    how would i know what goes into the view model?
 
